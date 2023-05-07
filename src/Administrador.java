c57b9385dc3be8b93eb52ecc1cf807529bfd11f9
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-05-07 17:28:00 +0200

    Add: clase administrador

---
+++ src/Administrador.java
@@ -0,0 +1,25 @@
+public class Administrador extends Usuario{
+    public boolean agregarCine() {
+        return true;
+    }
+
+    public boolean eliminarCine() {
+        return true;
+    }
+
+    public boolean agregarSala() {
+        return true;
+    }
+
+    public boolean eliminarSala() {
+        return true;
+    }
+
+    public boolean agregarPelicula() {
+        return true;
+    }
+
+    public boolean eliminarPelicula() {
+        return true;
+    }
+}
