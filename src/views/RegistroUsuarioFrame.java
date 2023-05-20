8dfcb24af47326cc51a3a4f0df32d3f1d03cb98e
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-20 18:42:03 +0200

    Configuramos la ventana en esta ocasión la llamamos frame

--- src/views/RegistroUsuarioFrame.java
+++ src/views/RegistroUsuarioFrame.java
@@ -1,6 +1,11 @@
 package views;
 import javax.swing.*;
-
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 public class RegistroUsuarioFrame {
     private JFrame frame;
     private JPanel panelIzquierdo;
@@ -10,4 +15,12 @@ public class RegistroUsuarioFrame {
     private JTextField correoTextField;
     private JPasswordField contrasenaPasswordField;
     private JButton registrarButton;
+
+    public RegistroUsuarioFrame() {
+        //Configuramos la ventana principal
+        frame = new JFrame("Registro de Usuario");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.DARK_GRAY);
+        frame.setLayout(new BorderLayout());
+    }
 }
