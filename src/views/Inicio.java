ee64b86c1a5388ba201fd32576a29d6467f9cc21
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-20 18:05:30 +0200

    Creamos la ventana principal

--- src/views/Inicio.java
+++ src/views/Inicio.java
@@ -1,4 +1,17 @@
 package views;
-
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.File;
+import java.io.IOException;
 public class Inicio {
+    public static void main(String[] args) {
+        //Se crea la ventana principal
+        JFrame ventana = new JFrame("CineScript");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.getContentPane().setBackground(Color.DARK_GRAY);
+
+    }
 }
