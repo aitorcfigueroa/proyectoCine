d457d60f218d2749c0fde77892ba177f809edda5
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-20 18:39:34 +0200

    Se añade la clase para la vista de un registro de usuario

---
+++ src/views/RegistroUsuarioFrame.java
@@ -0,0 +1,13 @@
+package views;
+import javax.swing.*;
+
+public class RegistroUsuarioFrame {
+    private JFrame frame;
+    private JPanel panelIzquierdo;
+    private JPanel panelDerecho;
+    private JPanel footerPanel;
+    private JTextField nombreTextField;
+    private JTextField correoTextField;
+    private JPasswordField contrasenaPasswordField;
+    private JButton registrarButton;
+}
