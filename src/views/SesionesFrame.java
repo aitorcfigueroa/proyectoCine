e2b582d861686e96a452b7b4c4afec954867c4c4
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-23 21:22:50 +0200

    Se añade la fuente predeterminada para el proyecto

--- src/views/SesionesFrame.java
+++ src/views/SesionesFrame.java
@@ -1,7 +1,12 @@
 package views;
 
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.io.File;
+import java.io.IOException;
 
 public class SesionesFrame extends JFrame {
 
@@ -10,6 +15,12 @@ public class SesionesFrame extends JFrame {
         setTitle("CineScript"); //título de la ventana
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //acción para cerrar la ventana
         getContentPane().setBackground(Color.DARK_GRAY); //color de fondo del contenido de la ventana
+
+        //Carga de la fuente Montserrat-Medium
+        Font montserratMedium = loadFont("C:\\Users\\Angela\\IdeaProjects\\proyectoCine\\resources\\Montserrat-Medium.ttf");
+
+        //Se establece la fuente para el proyecto
+        setUIFont(new FontUIResource(montserratMedium));
 
     }
 }
