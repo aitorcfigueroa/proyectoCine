805b95c418a67588ee8a87bfcdd2565936eb5bc5
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-23 21:17:37 +0200

    Se añade la configuración de la ventana de Sesiones, (título, cierre y color de fondo)

--- src/views/SesionesFrame.java
+++ src/views/SesionesFrame.java
@@ -1,6 +1,15 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
+
 public class SesionesFrame extends JFrame {
 
+    //Constructor
+    public SesionesFrame() {
+        setTitle("CineScript"); //título de la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //acción para cerrar la ventana
+        getContentPane().setBackground(Color.DARK_GRAY); //color de fondo del contenido de la ventana
+
+    }
 }
