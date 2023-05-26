f6fafc47cde5afef423453268cde802c886302bd
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-26 13:07:38 +0200

    Añadimos array que almacena las rutas de las imágenes de las películas

--- src/views/CarteleraFrame.java
+++ src/views/CarteleraFrame.java
@@ -4,4 +4,15 @@ import javax.swing.*;
 
 public class CarteleraFrame extends JFrame {
 
+    //Array que almacena las rutas de las imágenes
+    private static final String[] IMAGE_PATHS = {
+            "../resources/peli1.png",
+            "../resources/peli2.png",
+            "../resources/peli3.png",
+            "../resources/peli4.png",
+            "../resources/peli5.png",
+            "../resources/peli6.png"
+    };
+
+
 }
