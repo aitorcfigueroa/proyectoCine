2ca8540a944005eaf1b29581dd1b3ba7932821b1
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-26 12:29:45 +0200

    Configuramos la ventana

--- src/views/LoginFrame.java
+++ src/views/LoginFrame.java
@@ -1,6 +1,25 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginFrame extends JFrame {
+    private JPanel headerPanel;
+    private JPanel panelIzquierdo;
+    private JPanel panelDerecho;
+    private JPanel footerPanel;
+    private JTextField correoTextField;
+    private JPasswordField contrasenaPasswordField;
+    private JButton iniciarSesionButton;
+
+    public LoginFrame(){
+        //configuramos ventana
+        setTitle("Inicio de Sesión"); //título de la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //acción para cerrar la ventana
+        getContentPane().setBackground(Color.DARK_GRAY);//color de fondo
+        setLayout(new BorderLayout());
+
+
+    }
+
 }
