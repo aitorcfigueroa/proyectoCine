a462e53cfcc2741290d9a42c67951328909a392b
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-05-28 12:33:51 +0200

    Refactor: Se cambia el tipo de arraylist que se devuelve y se implementa la lógica para crearlo

--- src/controller/sesionesController.java
+++ src/controller/sesionesController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import clases.Sesion;
+import clases.SesionesPorSala;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,25 +9,30 @@ import static model.sesionesModel.getListaSesiones;
 
 public class sesionesController {
     /**
-     * Método para recuperar las sesiones del día actual.
-     * @param cine nombre del cine
-     * @return ArrayList con la lista de sesiones de ese día
-     */
-    public static ArrayList<Sesion> sesionesHoy(String cine) {
-        LocalDate hoy = LocalDate.now();
-        ArrayList<Sesion> sesiones = getListaSesiones(cine, hoy);
-        return sesiones;
-    }
-
-    /**
-     * Método para recuperar las sesiones de un día diferente al de hoy.
+     * Método para recuperar las sesiones de un día
      * @param cine nombre del cine
      * @param fecha día en el que se quiere buscar sesiones
      * @return ArrayList con la lista de sesiones para el día elegido
      */
-    public static ArrayList<Sesion> sesionesOtraFecha(String cine, LocalDate fecha) {
-        fecha = LocalDate.parse("2023-05-14");
-        ArrayList<Sesion> sesiones = getListaSesiones(cine, fecha);
-        return sesiones;
+    public static ArrayList<SesionesPorSala> sesiones(String cine, LocalDate fecha) {
+        if (fecha == null) {
+            fecha = LocalDate.now();
+        } else {
+            fecha = LocalDate.parse("2023-05-14");
+        }
+
+        ArrayList<Sesion> listaSesiones = getListaSesiones(cine, fecha);
+        ArrayList<SesionesPorSala> sesionesPorSala = null;
+        for (Sesion sesion: listaSesiones) {
+            for (SesionesPorSala sesionPorSala: sesionesPorSala) {
+                if (sesionPorSala.getSala() == sesion.getIdSala()) {
+                    sesionPorSala.setSesiones(sesion);
+                } else {
+                    sesionesPorSala.add(new SesionesPorSala(sesion.getIdSala(), sesion.getPelicula(), sesion));
+                }
+            }
+        }
+
+        return sesionesPorSala;
     }
 }
