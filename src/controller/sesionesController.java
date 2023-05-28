ad4f52ac94f4915d53f3636668a7a6c575e1f37a
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-05-28 10:29:48 +0200

    Add: Documentación en el controlador de sesiones

--- src/controller/sesionesController.java
+++ src/controller/sesionesController.java
@@ -7,12 +7,23 @@ import java.util.ArrayList;
 import static model.sesionesModel.getListaSesiones;
 
 public class sesionesController {
+    /**
+     * Método para recuperar las sesiones del día actual.
+     * @param cine nombre del cine
+     * @return ArrayList con la lista de sesiones de ese día
+     */
     public static ArrayList<Sesion> sesionesHoy(String cine) {
         LocalDate hoy = LocalDate.now();
         ArrayList<Sesion> sesiones = getListaSesiones(cine, hoy);
         return sesiones;
     }
 
+    /**
+     * Método para recuperar las sesiones de un día diferente al de hoy.
+     * @param cine nombre del cine
+     * @param fecha día en el que se quiere buscar sesiones
+     * @return ArrayList con la lista de sesiones para el día elegido
+     */
     public static ArrayList<Sesion> sesionesOtraFecha(String cine, LocalDate fecha) {
         fecha = LocalDate.parse("2023-05-14");
         ArrayList<Sesion> sesiones = getListaSesiones(cine, fecha);
