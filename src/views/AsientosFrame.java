a4deaf958ed89c7f60f9f7fc31fcd479f5ddea1d
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-30 08:49:59 +0200

    Se añaden matrices necsarias para implementar la selección de asientos

--- src/views/AsientosFrame.java
+++ src/views/AsientosFrame.java
@@ -3,4 +3,10 @@ package views;
 import javax.swing.*;
 
 public class AsientosFrame extends JFrame {
+
+    private JButton[][] asientos; //matriz para cuadrícula de botones
+
+    private boolean[][] ocupados; //matriz para indicar si el asiento está ocupado o no
+
+    private boolean[][] seleccionados; //matriz para indicar si el asiento está siendo seleccionado o no
 }
