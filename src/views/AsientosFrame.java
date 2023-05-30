d277570919e1e902369a4680617e817d72dca264
angelaserantes <aserantesreboreda@danielcastelao.org>
2023-05-30 08:53:56 +0200

    Se añaden el constructor y el inicio de configuración de la ventana

--- src/views/AsientosFrame.java
+++ src/views/AsientosFrame.java
@@ -1,6 +1,7 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AsientosFrame extends JFrame {
 
@@ -9,4 +10,12 @@ public class AsientosFrame extends JFrame {
     private boolean[][] ocupados; //matriz para indicar si el asiento está ocupado o no
 
     private boolean[][] seleccionados; //matriz para indicar si el asiento está siendo seleccionado o no
+
+    //constructor
+    public AsientosFrame(){
+        setTitle("Selección de Asientos"); //título de la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //acción para cerrar la ventana
+        getContentPane().setBackground(Color.DARK_GRAY); //color de fondo
+
+    }
 }
