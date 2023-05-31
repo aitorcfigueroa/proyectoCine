c27bea116b422665e8647644f0249908f7098ede
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-05-31 22:24:50 +0200

    Add: se crea nuevo método para recuperar una sesión por id

--- src/model/sesionesModel.java
+++ src/model/sesionesModel.java
@@ -23,7 +23,7 @@ import static model.salas.getListaSalas;
 
 public class sesionesModel {
     public static void main(String[] args) {
-        ArrayList<Sesion> sesionesCine = getListaSesiones("Castelao", LocalDate.parse("2023-05-14"));
+        Sesion sesionesCine = getSesion(1);
         System.out.println(sesionesCine);
     }
 
@@ -90,4 +90,58 @@ public class sesionesModel {
         closeConection(conexion);
         return sesionArrayList;
     }
+
+    /**
+     * Método para recuparar una sesión
+     * @param id id de la sesión
+     * @return una sesión
+     */
+    public static Sesion getSesion(int id) {
+        Sesion newSesion = null;
+        Connection conexion = dbconnection.conexion();
+        Statement seleccion = null;
+        String texto_seleccion = "select * from sesiones where idSesion=" + id;
+        ResultSet resultado = null;
+        int idSesion;
+        int idSala;
+        int idPelicula;
+        Pelicula pelicula;
+        LocalDate fechaBD;
+        LocalTime hora;
+        Map butacas;
+
+        try {
+            assert conexion != null;
+            seleccion = conexion.createStatement();
+            resultado = seleccion.executeQuery(texto_seleccion);
+            while(resultado.next()){
+                idSesion = resultado.getInt("idSesion");
+                idSala = resultado.getInt("idSala");
+                idPelicula = resultado.getInt("idPelicula");
+
+                pelicula = getPelicula(idPelicula);
+
+                fechaBD = LocalDate.parse(resultado.getString("fecha"));
+                hora = LocalTime.parse(resultado.getString("hora"));
+
+                String sitios = resultado.getString("butacas");
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                butacas = objectMapper.readValue(sitios, Map.class);
+
+                newSesion = new Sesion(idSesion, idSala, pelicula, fechaBD, hora, butacas);
+            }
+            resultado.close();
+        } catch (SQLException e) {
+            System.out.println("Error a la hora de consultar la tabla");
+            System.out.println(e.getLocalizedMessage());
+            return null;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        closeConection(conexion);
+        return newSesion;
+    }
+
+    //TODO: Update butacas.
 }
