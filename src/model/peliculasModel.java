c4143e9a1376b439bb0e9f6996862d4279120f13
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-06-04 10:35:32 +0200

    Add: Modelo para recuperar la lista de películas disponibles

--- src/model/peliculasModel.java
+++ src/model/peliculasModel.java
@@ -52,4 +52,46 @@ public class peliculasModel {
         closeConection(conexion);
         return nuevaPelicula;
     }
+
+    /**
+     * Modelo para recuperar la lista de películas
+     * @return un ArrayList con todas las películas de la base de datos
+     */
+    public static ArrayList<Pelicula> getListaPeliculas() {
+        Connection conexion = dbconnection.conexion();
+        Statement seleccion = null;
+        String texto_seleccion = "select * from peliculas";
+        ResultSet resultado = null;
+        Pelicula nuevaPelicula = null;
+        ArrayList<Pelicula> peliculas = new ArrayList<>();
+
+        try {
+            assert conexion != null;
+            seleccion = conexion.createStatement();
+            resultado = seleccion.executeQuery(texto_seleccion);
+            while(resultado.next()){
+                int idPelicula = resultado.getInt("idPelicula");
+                String titulo = resultado.getString("titulo");
+                String director = resultado.getString("director");
+                int año = resultado.getInt("año");
+                int duracion = resultado.getInt("duracion");
+                int edad = resultado.getInt("Edad");
+                String generos = resultado.getString("genero");
+                String sinopsis = resultado.getString("sinopsis");
+                String cartel = resultado.getString("cartel");
+
+                ArrayList<String> genero = new ArrayList<>(Arrays.asList(generos.split(",")));
+
+                nuevaPelicula = new Pelicula(idPelicula, titulo, director, año, duracion, edad, genero, sinopsis, cartel);
+                peliculas.add(nuevaPelicula);
+            }
+            resultado.close();
+        } catch (SQLException e) {
+            System.out.println("Error a la hora de consultar la tabla, puede que la película no exista.");
+            System.out.println(e.getLocalizedMessage());
+            return null;
+        }
+        closeConection(conexion);
+        return peliculas;
+    }
 }
